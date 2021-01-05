f177d5cd74a552eb8129599d35b0e96e58d768d1
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-04 19:31:34 -0800

    complete exercise 2, explore difference btwn find_by and where
    why prepend the variable names with @?
    => Variables that start with @ have instance scope.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+apparel = Store.where(mens_apparel: true, womens_apparel: true)
+
+
+@store1.name = "Burnababy"
+
+puts @store1.name
+puts apparel.count
