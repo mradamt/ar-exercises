9c19adb86355634deb7e59ce85f0707b01c6f747
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-04 21:09:32 -0800

    complete exercise 7 with custom validation for mens_ or womens_apparel

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :store, presence: true
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40 }
+  validates :hourly_rate, numericality: { only_integer: true, less_than_or_equal_to: 200 }
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_mens_or_womens_apparel
+
+  def must_carry_mens_or_womens_apparel
+    if !mens_apparel && !womens_apparel
+      mens_apparel || errors.add(:mens_apparel, ": store must stock one of mens_apparel or womens_apparel")
+      womens_apparel || errors.add(:womens_apparel, ": store must stock one of mens_apparel or womens_apparel")
+    end
+  end
 end
