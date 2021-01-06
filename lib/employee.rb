e644e279532f1e791cc0e338f39872aa1f6b0f00
Adam Tyler <mr.adamtyler@gmail.com>
2021-01-05 20:54:42 -0800

    exercise 8 repeat: generate_password using before_save WARNING
    before_save runs on every record update, this isn't desired behaviour here
    before_create runs only on record creation
    https://stackoverflow.com/questions/6249475/ruby-on-rails-callback-what-is-difference-between-before-save-and-before-crea

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,7 +4,7 @@ class Employee < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40 }
   validates :hourly_rate, numericality: { only_integer: true, less_than_or_equal_to: 200 }
-  before_create :generate_password
+  before_save :generate_password
 
   private
 
